6d2258e69d60720152648cc149a564c791882ebc
jimmy-source <jimmy-1991@live.cn>
2013-09-27 16:03:25 +0800

    Update tkinter_label_01.py

--- tkinter_label_01.py
+++ tkinter_label_01.py
@@ -3,6 +3,6 @@
 from Tkinter import *
 
 window_01 = Tk()
-label_01 = Label(window_01,text='i wanna fuck you,chang huiwen')
+label_01 = Label(window_01,text='i wanna meet you on sat')
 label_01.pack()
 mainloop()
